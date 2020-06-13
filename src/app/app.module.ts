8ac6e93cd28fbe2bf34d3f3351a77287825d805a
Alexandr Danilyuk <adanilyuk@whitebox.ru>
2020-06-13 10:55:44 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MqttModule, IMqttServiceOptions } from "ngx-mqtt";
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'wqtt.ru',
+  port: 3158,
+  protocol: 'wss',
+  path: '/mqtt',
+  clientId: '1234e3qer23rf',
+  username: 'u_7BA38T',
+  password: 'zddE2SRb',
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
